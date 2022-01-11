1f5f6efd8699372eaf35e4eeb52c72f68f1505ee
DvorakDwarf <96934612+DvorakDwarf@users.noreply.github.com>
2022-01-10 21:07:12 -0800

    Add files via upload

---
+++ csvTranslator.py
@@ -0,0 +1,35 @@
+import numpy as np
+import csv
+from sklearn.utils import Bunch
+
+def load_trolley_test():
+    with open(r'E:/Code/Code_projects/TrolleyProblemStuff/TestTrolley.csv') as csv_file:
+        data_reader = csv.reader(csv_file)
+        feature_names = next(data_reader)[:-1]
+        data = []
+        target = []
+        for row in data_reader:
+            features = row[:-1]
+            label = row[-1]
+            data.append([float(num) for num in features])
+            target.append(int(label))
+        
+        data = np.array(data)
+        target = np.array(target)
+    return Bunch(data=data, target=target, feature_names=feature_names)
+
+def load_trolley_short():
+    with open(r'E:/Code/Code_projects/TrolleyProblemStuff/TrolleyShort.csv') as csv_file:
+        data_reader = csv.reader(csv_file)
+        feature_names = next(data_reader)[:-1]
+        data = []
+        target = []
+        for row in data_reader:
+            features = row[:-1]
+            label = row[-1]
+            data.append([float(num) for num in features])
+            target.append(int(label))
+        
+        data = np.array(data)
+        target = np.array(target)
+    return Bunch(data=data, target=target, feature_names=feature_names)
